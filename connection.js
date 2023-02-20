3b549de90001d49b66ff02f57662b0a5074533c2
vincent <mightyme@qq.com>
2023-02-20 15:42:22 +0800

    feat: v1

---
+++ connection.js
@@ -0,0 +1,66 @@
+async function main(callback) {
+  const mongoose = require("mongoose");
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const Model = mongoose.model(
+      "books",
+      new mongoose.Schema({
+        title: { type: String, required: true },
+        comments: { type: Array },
+      })
+    );
+
+    const FindDoc = (filters, done) => {
+      Model.find(filters, (err, data) => {
+        if (err) return done(err, null);
+        done(null, data);
+      });
+    };
+
+    const DeleteDoc = (filters, done) => {
+      Model.deleteMany(filters, (err, data) => {
+        if (err) return done(err, null);
+        done(null, data);
+      });
+    };
+
+    const AddDoc = (params, done) => {
+      const d = new Model(params);
+      d.save((err, doc) => {
+        if (err) return done(err, null);
+        done(null, doc);
+      });
+    };
+
+    const UpdateDoc = (_id, comment, done) => {
+      //   console.log(issue_id, params);
+      Model.findOne({ _id }, (err, doc) => {
+        console.log("findOne", err, doc);
+        if (err || !doc) return done(err, null);
+        doc.comments.push(comment);
+        doc.save((err1, doc1) => {
+          console.log("doc.save", err1, doc1);
+          if (err1) return console.error(err1);
+          done(null, doc1);
+        });
+      });
+    };
+
+    callback({
+      FindDoc,
+      DeleteDoc,
+      UpdateDoc,
+      AddDoc,
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Unable to Connect to Database");
+  }
+}
+
+module.exports = main;
--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,104 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-module.exports = function (app) {
+module.exports = function (app, client) {
+  const formatDoc = (doc) => {
+    const res = doc._doc;
+    res["commentcount"] = doc.comments.length;
+    return res;
+  };
 
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      client.FindDoc({}, (err, docs) => {
+        if (err) return next(err);
+        docs = docs.map((doc) => formatDoc(doc));
+        res.send(docs);
+      });
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
+      let params = { title, comments: [] };
+      client.AddDoc(params, (err, doc) => {
+        if (err) {
+          res.status(200);
+          res.send({ error: "required field(s) missing" });
+          return;
+        }
+        res.send(formatDoc(doc));
+      });
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
+      client.DeleteDoc({}, (err, doc) => {
+        if (err || doc.deletedCount == 0) {
+          res.send({ error: "no book exists" });
+          return;
+        }
+        res.send({
+          result: "delete successful",
+        });
+      });
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
+      let _id = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      client.FindDoc({ _id }, (err, docs) => {
+        if (err || docs.length == 0) {
+          res.send("no book exists");
+          return;
+        }
+        let doc = docs[0];
+        res.send(formatDoc(doc));
+      });
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
+
+    .post(function (req, res) {
+      let _id = req.params.id;
       let comment = req.body.comment;
+      if (comment == undefined) {
+        res.send("missing required field comment");
+        return;
+      }
+
       //json res format same as .get
+      client.UpdateDoc(_id, comment, (err, doc) => {
+        if (err || !doc) {
+          res.send("no book exists");
+          return;
+        }
+        res.send(formatDoc(doc));
+      });
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
+
+    .delete(function (req, res) {
+      let _id = req.params.id;
       //if successful response will be 'delete successful'
+      client.DeleteDoc({ _id }, (err, doc) => {
+        console.log("delete book:", err, doc);
+        if (err || doc.deletedCount == 0) {
+          res.send({ error: "no book exists" });
+          return;
+        }
+        res.send({
+          result: "delete successful",
+        });
+      });
     });
-  
 };
